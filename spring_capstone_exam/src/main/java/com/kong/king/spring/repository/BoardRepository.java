70b5829d387cb5a0ba8e3183357ba685fd7896db
sinsungs <39232875+sinsungs@users.noreply.github.com>
2023-04-16 01:46:46 +0900

    Delete spring_capstone_exam directory

--- spring_capstone_exam/src/main/java/com/kong/king/spring/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kong.king.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kong.king.spring.entity.Board;
-
-public interface BoardRepository extends JpaRepository<Board, String> {
-
-}
--- spring_capstone_exam/src/main/java/com/kong/king/spring/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kong.king.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kong.king.spring.entity.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
